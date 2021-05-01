1288baaa09cd992c7ec678f3296d181291bbdc6e
jason1794c <69557789+jason1794c@users.noreply.github.com>
2021-05-01 18:12:14 -0400

    Update placeholder text

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -13,7 +13,7 @@ function SearchBar() {
     return (
         <input
             type='text'
-            placeholder='Search'
+            placeholder='Search for movies'
             value={search}
             onChange={e => handleSearch(e)}
         />
